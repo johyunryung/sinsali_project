6d2b92ebb0f9ceb5503fbe4b3d84b809a3104628
hyun Ryung <s2019w37@e-mirim.hs.kr>
2020-11-14 20:21:33 +0900

    신사리 프로젝트

---
+++ app/src/main/java/kr/hs/emirim/s2019w37/sinsali_project/SpashActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.s2019w37.sinsali_project
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SpashActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+    }
+}
